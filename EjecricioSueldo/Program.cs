7755962ceba0f5c4efe75f4aa9af853caa39abf1
jvelazquez <jonathanvgms@gmail.com>
2020-09-29 18:22:12 -0300

    Ejercicio Sueldo

---
+++ EjecricioSueldo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EjecricioSueldo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese nombre: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Ingrese numero: ");
+            int numeroVendedor = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese Sueldo Basico: ");
+            double sueldoBasico = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese Sueldo Extra: ");
+            double sueldoExtra = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese Bono: ");
+            var bono = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese cantidad ventas: ");
+            int cantVentas = Convert.ToInt32(Console.ReadLine());
+
+            Vendedor vendedora;
+
+            vendedora = new Vendedor(nombre, numeroVendedor, sueldoBasico, sueldoExtra, bono, cantVentas);
+
+            vendedora.InformarSueldo();
+        }
+    }
+}
---
+++ EjecricioSueldo/Vendedor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjecricioSueldo
+{
+    public class Vendedor
+    {
+        public string Nombre { get; set; }
+
+        public int Numero { get; set; }
+
+        public double SueldoBasico { get; }
+        
+        public double SueldoExtra { get; }
+        
+        public double Bono { get; }
+
+        public int CantVentas { get; set; }
+
+        public Vendedor(string nombre, int numero, double sueldoBasico, double sueldoExtra, double bono, int cantVentas)
+        {
+            Nombre = !string.IsNullOrEmpty(nombre) ? nombre : throw new Exception("El nombre no puede ser cadena vacia");
+           
+            Numero = numero;
+            
+            if (sueldoBasico > 0)
+                SueldoBasico = sueldoBasico;
+            else
+                throw new Exception("El sueldo basico es incorrecto");
+            
+            SueldoExtra = sueldoExtra > 0 ? sueldoExtra : throw new Exception("El sueldo extra es incorrecto");
+
+            Bono = bono > 0 ? bono : throw new Exception("El bono es incorrecto");
+
+            CantVentas = cantVentas >= 0 ? cantVentas : throw new Exception("Cantidad de ventas incorrecta");
+        }
+
+        public void InformarSueldo()
+        {
+            Console.WriteLine($"El sueldo de {Nombre} es: {CalcularSueldo()}");
+        }
+
+        private double CalcularSueldo() => SueldoBasico + SueldoExtra + Bono * CantVentas;
+        
+        //private double CalcularSueldo()
+        //{
+        //    return SueldoBasico + SueldoExtra + Bono * CantVentas;
+        //}
+    }
+}
